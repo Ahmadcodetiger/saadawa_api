5ee501e6ccf844d0715a0f348fefc040b3291d1e
devAhmad <ahmadhussain554@gmail.com>
2026-03-22 10:47:22 +0000

    Update response.d.ts

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -1,3 +1,11 @@
+import { Request } from 'express';
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: string;
+    role?: string;
+  };
+}
 declare module '../utils/response' {
   export class ApiResponse {
     static success(res: any, message: string, data?: any): any;
